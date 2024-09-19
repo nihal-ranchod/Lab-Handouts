96042e940f30080ce002b922841d977f7c707c33
nihal-ranchod <nihal.ranchod@gmail.com>
2024-09-19 12:09:57 +0200

    Add: last TODO in train atari

--- Lab3/starter-code/train_atari.py
+++ Lab3/starter-code/train_atari.py
@@ -53,51 +53,60 @@ if __name__ == "__main__":
                      hyper_params["discount-factor"])
     agent.act(env.observation_space.sample())
 
-    # eps_timesteps = hyper_params["eps-fraction"] * float(hyper_params["num-steps"])
-    # episode_rewards = [0.0]
+    eps_timesteps = hyper_params["eps-fraction"] * float(hyper_params["num-steps"])
+    episode_rewards = [0.0]
 
-    # state = env.reset()
-    # for t in range(hyper_params["num-steps"]):
-    #     # _ = env.step(env.action_space.sample())
-    #     fraction = min(1.0, float(t) / eps_timesteps)
-    #     eps_threshold = hyper_params["eps-start"] + fraction * (
-    #         hyper_params["eps-end"] - hyper_params["eps-start"]
-    #     )
-    #     sample = random.random()
-    #     # TODO
-    #     #  select random action if sample is less equal than eps_threshold
-    #     # take step in env
-    #     # add state, action, reward, next_state, float(done) to reply memory - cast done to float
-    #     # add reward to episode_reward
+    state = env.reset()
+    for t in range(hyper_params["num-steps"]):
+        # _ = env.step(env.action_space.sample())
+        fraction = min(1.0, float(t) / eps_timesteps)
+        eps_threshold = hyper_params["eps-start"] + fraction * (
+            hyper_params["eps-end"] - hyper_params["eps-start"]
+        )
+        sample = random.random()
+        # TODO
+        #  select random action if sample is less equal than eps_threshold
+        # take step in env
+        # add state, action, reward, next_state, float(done) to reply memory - cast done to float
+        # add reward to episode_reward
 
-    #     episode_rewards[-1] += reward
-    #     if done:
-    #         state = env.reset()
-    #         episode_rewards.append(0.0)
+        # if random.random() < eps_threshold:
+        #     action = env.action_space.sample()
+        # else:
+        #     action = agent.act(state)
 
-    #     if (
-    #         t > hyper_params["learning-starts"]
-    #         and t % hyper_params["learning-freq"] == 0
-    #     ):
-    #         agent.optimise_td_loss()
+        # next_state, reward, done, _ = env.step(action)
+        # replay_buffer.add(state, action, reward, next_state, float(done))
+        #state = next_state
 
-    #     if (
-    #         t > hyper_params["learning-starts"]
-    #         and t % hyper_params["target-update-freq"] == 0
-    #     ):
-    #         agent.update_target_network()
+        episode_rewards[-1] += reward
+        if done:
+            state = env.reset()
+            episode_rewards.append(0.0)
 
-    #     num_episodes = len(episode_rewards)
+        if (
+            t > hyper_params["learning-starts"]
+            and t % hyper_params["learning-freq"] == 0
+        ):
+            agent.optimise_td_loss()
 
-    #     if (
-    #         done
-    #         and hyper_params["print-freq"] is not None
-    #         and len(episode_rewards) % hyper_params["print-freq"] == 0
-    #     ):
-    #         mean_100ep_reward = round(np.mean(episode_rewards[-101:-1]), 1)
-    #         print("********************************************************")
-    #         print("steps: {}".format(t))
-    #         print("episodes: {}".format(num_episodes))
-    #         print("mean 100 episode reward: {}".format(mean_100ep_reward))
-    #         print("% time spent exploring: {}".format(int(100 * eps_threshold)))
-    #         print("********************************************************")
+        if (
+            t > hyper_params["learning-starts"]
+            and t % hyper_params["target-update-freq"] == 0
+        ):
+            agent.update_target_network()
+
+        num_episodes = len(episode_rewards)
+
+        if (
+            done
+            and hyper_params["print-freq"] is not None
+            and len(episode_rewards) % hyper_params["print-freq"] == 0
+        ):
+            mean_100ep_reward = round(np.mean(episode_rewards[-101:-1]), 1)
+            print("********************************************************")
+            print("steps: {}".format(t))
+            print("episodes: {}".format(num_episodes))
+            print("mean 100 episode reward: {}".format(mean_100ep_reward))
+            print("% time spent exploring: {}".format(int(100 * eps_threshold)))
+            print("********************************************************")
